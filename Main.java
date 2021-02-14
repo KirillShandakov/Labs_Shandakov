07c5e1d663672a7e8c3a4b7596acc31f847d96e5
KirillShandakov <77574632+KirillShandakov@users.noreply.github.com>
2021-02-14 18:19:04 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input k:");
+        int k = in.nextInt();
+        System.out.println("Input n:");
+        int n = in.nextInt();
+        double otvet = Math.sqrt(k * n);
+        for(int i = (n - 1); i != 0; i--){
+            otvet = Math.sqrt((k * i) + otvet);
+        }
+        System.out.printf("%.3f", otvet);
+        }
+    }
